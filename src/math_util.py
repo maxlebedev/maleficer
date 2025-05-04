e261e77424a57cc1b0346c7106b0b941630c9a78
Max Lebedev <maxlebedev@live.com>
2025-05-04 11:30:51 -0400

    forgot to add math_util

---
+++ src/math_util.py
@@ -0,0 +1,14 @@
+import esper
+
+import components as cmp
+
+
+def clamp(num: int, high: int, low=0):
+    """clamp a number between high and low"""
+    return min(high, max(low, num))
+
+
+def apply_damage(entity: int, value: int):
+    hp = esper.component_for_entity(entity, cmp.Health)
+    hp.current -= value
+    hp.current = clamp(hp.current, hp.max)
